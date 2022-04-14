525f4ea5e50672c46140a40dece74a9e92a0c398
Spencer King <spencerd.king@gmail.com>
2022-04-13 22:34:58 -0400

    Main WHR dataset from 2015-2021 is ready for modeling and analysis

--- bia_tp.R
+++ bia_tp.R
@@ -6,44 +6,69 @@ install.packages("tidyverse")
 library(tidyverse)
 
 ############################################################
-## MASTER HAPPINESS AGGREGATED DATA SET
+## WHR AGGREGATED DATA SET 2015 - 2021 
 ############################################################
 
 ### Data Exploration
 
 # Reading in project data from CSV file
-whr_15 <- read_csv("./data/whr15_21/2015.csv")
-whr_16 <- read_csv("./data/whr15_21/2016.csv")
-whr_17 <- read_csv("./data/whr15_21/2017.csv")
-whr_18 <- read_csv("./data/whr15_21/2018.csv")
-whr_19 <- read_csv("./data/whr15_21/2019.csv")
-whr_20 <- read_csv("./data/whr15_21/2020.csv")
-whr_21 <- read_csv("./data/whr15_21/2021.csv")
-
-
-# Cleaning for columns of interest
-
-col_names <- names(whr_18)
-whr_15c <- whr_15 %>% select (col_names)
-
-
-# Merging all dataframes
-whr_full <- do.call("rbind", list(whr_15, whr_16, whr_17, whr_18, whr_19, whr_20, whr_21))
-
-
-
-
+whr_raw <- read_csv("./data/whr_15-21_mc.csv")
 
 # Checking how many NA value each data column has
-nrow(data_raw)
-colSums(is.na(data_raw))
+nrow(whr_raw)
+colSums(is.na(whr_raw))
 
-no_na <- na.omit(data_raw)
+# Checking which countries are represented in all years
+c_uniq <- unique(whr_raw$Country)
+length(c_uniq)
+
+c_occ <- list()
+
+for (c1 in c_uniq) {
+  
+  inc = 0
+  
+  for (c2 in whr_raw$Country) {
+    
+    if (c1 == c2) {
+      inc = inc + 1
+    }
+    
+  }
+  
+  c_occ <- append(c_occ, inc)
+  
+}
+
+c_occ 
+
+# Cleaning countries that are not represented in all years
+
+inc = 0
+whr_raw_c <- whr_raw
+c_occ_f <- list()
+
+for (each in c_occ) {
+  
+  if (each != 7) {
+    whr_raw_c <- subset(whr_raw_c, Country != c_uniq[inc])
+  }
+  
+  if (each == 7) {
+    c_occ_f <- append(c_occ_f, c_uniq[inc])
+  }
+  
+  inc = inc + 1
+  
+}
+
+# Checking to make sure cleaning was successful
 
 
-### Data Cleaning
+length(c_occ_f)
+length(unique(whr_raw_c$Country))
 
 
-############################################################
-## MASTER HAPPINESS AGGREGATED DATA SET
+
+
 ############################################################
